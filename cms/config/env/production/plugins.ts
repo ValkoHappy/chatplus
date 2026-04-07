fe088518e55435921e6302ea9480e68d6937d8ea
Alexey <ValkoHappy@users.noreply.github.com>
2026-04-07 14:10:12 +0300

    Fix local upload production plugin config

--- cms/config/env/production/plugins.ts
+++ cms/config/env/production/plugins.ts
@@ -1,34 +1,38 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
-  upload: env('UPLOAD_PROVIDER', 'local') === 'aws-s3'
-    ? {
-        config: {
-          provider: 'aws-s3',
-          providerOptions: {
-            baseUrl: env('CDN_URL', undefined),
-            rootPath: env('CDN_ROOT_PATH', undefined),
-            s3Options: {
-              accessKeyId: env('AWS_ACCESS_KEY_ID'),
-              secretAccessKey: env('AWS_ACCESS_SECRET'),
-              region: env('AWS_REGION'),
-              endpoint: env('AWS_ENDPOINT', undefined),
-              forcePathStyle: env.bool('AWS_FORCE_PATH_STYLE', false),
-              params: {
-                ACL: env('AWS_ACL', 'public-read'),
-                signedUrlExpires: env.int('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-                Bucket: env('AWS_BUCKET'),
-              },
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => {
+  if (env('UPLOAD_PROVIDER', 'local') !== 'aws-s3') {
+    return {};
+  }
+
+  return {
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          baseUrl: env('CDN_URL', undefined),
+          rootPath: env('CDN_ROOT_PATH', undefined),
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            endpoint: env('AWS_ENDPOINT', undefined),
+            forcePathStyle: env.bool('AWS_FORCE_PATH_STYLE', false),
+            params: {
+              ACL: env('AWS_ACL', 'public-read'),
+              signedUrlExpires: env.int('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+              Bucket: env('AWS_BUCKET'),
             },
           },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
         },
-      }
-    : undefined,
-});
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  };
+};
 
 export default config;
